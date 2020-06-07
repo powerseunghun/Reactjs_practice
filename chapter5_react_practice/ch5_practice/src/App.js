8660b37ee5d8e28c379488f1af1e09a47562ba20
powerseunghun <size411@naver.com>
2020-06-07 17:09:49 +0900

    Ref with callBack_Function()

--- chapter5_react_practice/ch5_practice/src/App.js
+++ chapter5_react_practice/ch5_practice/src/App.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import ValidationSample from "./ValidationSample";
 //import CreateRef from './CreateRef';
+import ValidationSample2 from "./ValidationSample2";
 
 class App extends Component {
   render() {
     return (
-      <ValidationSample/>
+      <ValidationSample2/>
     )
   }
 }
