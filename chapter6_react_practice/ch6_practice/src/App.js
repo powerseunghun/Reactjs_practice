e55de58238be476d9fa6f328d907e3f1cd5f6ae6
powerseunghun <size411@naver.com>
2020-06-11 21:22:35 +0900

    Component loop Map()

---
+++ chapter6_react_practice/ch6_practice/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import IterationSample from './IterationSample';
+
+// const App = () => {
+//   return (
+//     <IterationSample/>
+//   )
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <IterationSample/>
+    )
+  }
+}
+
+export default App;
