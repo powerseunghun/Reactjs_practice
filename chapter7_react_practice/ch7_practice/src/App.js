bcaf53f3df5b5599bc14ad37169dde3aaa1d2e8b
powerseunghun <size411@naver.com>
2020-06-13 20:42:44 +0900

    use lifeCycleMethod()

--- chapter7_react_practice/ch7_practice/src/App.js
+++ chapter7_react_practice/ch7_practice/src/App.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
+import LifeCycleSample from './LifeCycleSample';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return(
       <div>
-        
+        <button onClick={this.handleClick}>
+          random color
+        </button>
+        <LifeCycleSample color={this.state.color}/>
       </div>
     )
   }
