b707f63e22963a6c548ad511318c2b9e6d3ebb7a
powerseunghun <size411@naver.com>
2020-06-21 17:25:09 +0900

    use Scss

--- chapter9_react_practice/ch9_practice/src/App.js
+++ chapter9_react_practice/ch9_practice/src/App.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import SassComponent from './SassComponent.js';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header>
+//           <img src={logo} className="logo" alt="logo"/>
+//           <p>
+//             Edit <code>src/App.js</code> and Save to reload.
+//           </p>
+//           <a
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          <img src={logo} className="logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.js</code> and Save to reload.
-          </p>
-          <a
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <SassComponent/>
       </div>
-    );
+    )
   }
 }
 
