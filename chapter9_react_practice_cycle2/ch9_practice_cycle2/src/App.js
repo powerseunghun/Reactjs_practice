5b09dbbd26dd6e1b4576b098a08d2350d7f1eeaf
powerseunghun <size411@naver.com>
2020-07-29 00:59:44 +0900

    use Sass

---
+++ chapter9_react_practice_cycle2/ch9_practice_cycle2/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// 9.2 use Sass
+import SassComponent from './SassComponent';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ chapter9_react_practice_cycle2/ch9_practice_cycle2/src/SassComponent.js
@@ -0,0 +1,18 @@
+// 9.2 use Sass
+import React from 'react';
+import './SassComponent.scss';
+
+const SassComponent = () => {
+    return (
+        <div className="SassComponent">
+            <div className="box red"/>
+            <div className="box orange"/>
+            <div className="box yellow"/>
+            <div className="box green"/>
+            <div className="box blue"/>
+            <div className="box indigo"/>
+            <div className="box violet"/>
+        </div>
+    );
+}
+export default SassComponent;
