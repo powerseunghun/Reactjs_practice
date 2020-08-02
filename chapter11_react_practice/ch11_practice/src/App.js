4dc432cb01ccd8258a87bb6898313ef52dfc9cf6
powerseunghun <size411@naver.com>
2020-08-03 01:57:30 +0900

    Rendering A lot of data for optimization

---
+++ chapter11_react_practice/ch11_practice/src/App.js
@@ -0,0 +1,65 @@
+import React, { useState, useRef, useCallback } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
+import { arrayContaining } from '../node_modules/expect/build/index';
+
+const App = () => {
+  const [todos, setTodos] = useState(createBulkTodos);
+
+  // unique value id
+  // use ref.. variable in
+  const nextId = useRef(2501);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos],
+  );
+
+  const onRemove = useCallback(
+    id => {
+      setTodos(todos.filter(todo => todo.id !== id));
+    },
+    [todos],
+  );
+
+  const onToggle = useCallback(
+    id => {
+      setTodos(
+        todos.map(todo => 
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+          ),
+      );
+    },
+    [todos],
+  );
+
+  // 11.1 Rendering a lot of data
+  function createBulkTodos() {
+    const array = [];
+    for (let i = 1; i <= 2500; i++) {
+      array.push({
+        id: i,
+        text: `To do ${i}`,
+        checked: false,
+      });
+    }
+    return array;
+  }
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert}/>
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle}/>
+    </TodoTemplate>
+  )
+}
+
+export default App;
