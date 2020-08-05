85f2fdc8a5dee43d409a108aa45a2caa341da202
powerseunghun <size411@naver.com>
2020-08-06 00:09:46 +0900

    use immer library

--- chapter12_react_practice/immer-tutorial/src/App.js
+++ chapter12_react_practice/immer-tutorial/src/App.js
@@ -1,4 +1,6 @@
 import React, { useRef, useCallback, useState } from 'react';
+// 12.1.4 immer Apply to App Component
+import produce from 'immer';
 
 const App = () => {
   const nextId = useRef(1);
@@ -12,10 +14,23 @@ const App = () => {
   const onChange = useCallback(
     e => {
       const { name, value } = e.target;
-      setForm({
-        ...form,
-        [name] : [value]
-      });
+      // setForm({
+      //   ...form,
+      //   [name] : [value]
+      // });
+      // 12.1.4 immer Apply to App Component
+      // setForm(
+      //   produce(form,draft => {
+      //     draft[name] = value;
+      //   })
+      // );
+
+      // 12.1.5 use immer togetehr useState's functional update
+      setForm(
+        produce(draft => {
+          draft[name] = value;
+        })
+      )
     },
     [form]
   );
@@ -31,10 +46,23 @@ const App = () => {
       };
 
       // new content registration to array
-      setData({
-        ...data,
-        array: data.array.concat(info)
-      });
+      // setData({
+      //   ...data,
+      //   array: data.array.concat(info)
+      // });
+      // 12.1.4 Apply immer to App Component
+      // setData(
+      //   produce(data, draft => {
+      //     draft.array.push(info);
+      //   })
+      // )
+
+      // 12.1.5 use immer togetehr useState's functional update
+      setData(
+        produce(draft => {
+          draft.array.push(info);
+        })
+      )
 
       // form initialize
       setForm({
@@ -43,16 +71,30 @@ const App = () => {
       });
       nextId.current += 1;
     },
-    [data, form.data, form.username]
+    [data, form.name, form.username]
   );
 
   // Function for category remove
   const onRemove = useCallback(
     id => {
-      setData({
-        ...data,
-        array: data.array.filter(info => info.id !== id)
-      });
+      // setData({
+      //   ...data,
+      //   array: data.array.filter(info => info.id !== id)
+      // });
+      
+      // 12.1.4 immer Apply to App Component
+      // setData(
+      //   produce(data, draft=>{
+      //     draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
+      //   })
+      // )
+
+      // 12.1.5 use immer togetehr useState's functional update
+      setData(
+        produce(draft => {
+          draft.array.splice(draft.array.findIndex(info => info.id ===id), 1);
+        })
+      )
     },
     [data]
   );
